a70a5499cd84a398018cbca6a265d0a1cd5fb97b
emre gemici <gmcc.emree@gmail.com>
2021-11-29 19:50:04 +0300

    Add project files.

---
+++ MCD_EncapsulationOrnekk/Daire.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_EncapsulationOrnekk
+{
+    class Daire
+    {
+        #region Özellikler
+        public double Yaricap { get; set; }
+        private double _cap = 0;
+
+        public double Cap
+        {
+            get
+            {
+                if (Yaricap >0d)
+                {
+                    _cap = Yaricap * 2;
+                }
+                return _cap;
+            }
+            set
+            {
+
+            }
+        }
+        public double PiSayisi { get; set; }
+
+        #endregion 
+
+        #region Constructorlar Yani Yapıcı Metotlar
+
+        public Daire()
+        {
+            PiSayisi = 3;
+        }
+
+        public Daire(double DisardanAlinanPiSayisi)
+        {
+            PiSayisi = DisardanAlinanPiSayisi;
+        }
+        #endregion
+
+        #region  Metotlar
+        public void AlanHesapla()
+        {
+            double sonuc = PiSayisi * Math.Pow(Yaricap, 2);
+            Console.WriteLine("Alan= "+sonuc .ToString ("0.00"));
+        }
+
+        public void CevreHesapla() 
+        {
+            double sonuc = 2 * PiSayisi * Yaricap;
+            Console.WriteLine("Çevre= "+sonuc.ToString ("0.00"));
+        }
+        #endregion 
+
+        
+    }
+}
---
+++ MCD_EncapsulationOrnekk/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_EncapsulationOrnekk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Daire daire1 = new Daire(3);
+            daire1.Yaricap = 5;
+            Console.WriteLine("Çap: " +daire1 .Cap .ToString ());
+            daire1.AlanHesapla();
+            daire1.CevreHesapla();
+
+            Console.ReadKey();
+        }
+    }
+}
